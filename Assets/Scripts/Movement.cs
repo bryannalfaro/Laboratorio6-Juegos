af3e4075394d80770e4a4fd6647e49a6d19ed502
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-05-04 13:00:00 -0600

    laboratorio completo

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody2D rgb2d;
+    private int score=0;
+    public Sprite mario2;
+    BoxCollider2D box;
+    public AudioClip powerUP;
+    public AudioClip jumpSound;
+    public AudioClip deathEnemy;
+    public AudioClip marioDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rgb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        Vector3 newScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            newScale.x = -5.0f;
+        }else if (Input.GetAxis("Horizontal") > 0.0f)
+        {
+            newScale.x = 5.0f;
+        }
+
+        transform.localScale = newScale;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rgb2d)
+        {
+            rgb2d.AddForce(new Vector2(Input.GetAxis("Horizontal") * 5,0));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PowerUp"))
+        {
+            score++;
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.PlayOneShot(powerUP);
+            Destroy(collision.gameObject);
+            gameObject.GetComponent<SpriteRenderer>().sprite = mario2;
+            box = GetComponent<BoxCollider2D>();
+            box.size = new Vector2(0.14f, 0.3f);
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (score < 1)
+            {
+               
+                Destroy(gameObject);
+                
+            }
+            else
+            {
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.PlayOneShot(deathEnemy);
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
+    void Jump()
+    {
+        if (rgb2d)
+        {
+            if (Mathf.Abs(rgb2d.velocity.y) < 0.05f)
+            {
+                AudioSource audio = GetComponent<AudioSource>();
+                audio.PlayOneShot(jumpSound);
+                rgb2d.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    
+}
